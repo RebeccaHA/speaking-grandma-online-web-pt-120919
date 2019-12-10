91b9cacbe1af1c402a16e90c8d9c72cdd92e8b4a
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-10 20:08:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 
 def speak_to_grandma(phrase)
   if phrase==phrase.upcase
-    puts 'HUH?! SPEAK UP, SONNY!'
+    p 'HUH?! SPEAK UP, SONNY!'
   elsif phrase =='I LOVE YOU GRANDMA!'
-    puts 'I LOVE YOU TOO PUMPKIN!'
+    p 'I LOVE YOU TOO PUMPKIN!'
    else phrase 
-    puts 'NO, NOT SINCE 1938!'
+    p 'NO, NOT SINCE 1938!'
   end
 end
 
